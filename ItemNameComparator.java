f2b4745eef7b6bf3ab9760060b47d9ec00d7a03f
PRERNA MISHRA <mprerna802@gmail.com>
2025-10-11 15:42:22 +0530

    Update ItemNameComparator.java

--- ItemNameComparator.java
+++ ItemNameComparator.java
@@ -6,9 +6,9 @@ public class ItemNameComparator implements Comparator<Item>{
 
 	@Override
 	public int compare(Item o1, Item o2) {
-		// TODO Auto-generated method stub
 		return o1.getName().compareTo(o2.getName());
 	}
 	
 
 }
+
